9ad4bdbd2b56e0846e80a32a719c722a7217b4e3
SidM77 <siddanth.manoj@gmail.com>
2023-03-13 21:36:17 +0530

    Added cosmetic changes
    Added Cosmetic Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,31 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
-        <a href="/">Sid's Ecom</a>
+        <a className="companyName" href="/">
+          SIDINO COUTURE
+        </a>
       </header>
-      <main>list products</main>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {data.products.map((product) => (
+          <div className="product" key={product.slug}>
+            <a href={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+            </a>
+            <div className="product-info">
+              {/* <a href={`/product/${product.slug}`}> */}
+              <p className="brandName">{product.brand}</p>
+              <p className="productName">{product.name}</p>
+              {/* </a> */}
+              <p className="productCost">₹{product.price}</p>
+              <button>Add to Cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
